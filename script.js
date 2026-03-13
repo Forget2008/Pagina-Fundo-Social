41280e9fd46ba501a027f3870a8eff26711b58be
NeoC0der <143536178+Forget2008@users.noreply.github.com>
2026-03-13 13:06:49 -0300

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-const counters = document.querySelectorAll(".counter h3")
+const counters = document.querySelectorAll('.counter')
 
-counters.forEach(counter => {
+counters.forEach(counter=>{
 
-counter.innerText = "0"
+const update=()=>{
 
-const update = () => {
+const target=+counter.getAttribute('data-target')
 
-const target = +counter.getAttribute("data-target")
-const c = +counter.innerText
+const count=+counter.innerText
 
-const increment = target / 200
+const increment=target/200
 
-if(c < target){
-counter.innerText = `${Math.ceil(c + increment)}`
+if(count<target){
+
+counter.innerText=Math.ceil(count+increment)
+
 setTimeout(update,10)
+
 }else{
-counter.innerText = target
+
+counter.innerText=target
+
 }
 
 }
@@ -25,40 +29,15 @@ update()
 })
 
 
-
-const observer = new IntersectionObserver(entries => {
-
-entries.forEach(entry => {
-
-if(entry.isIntersecting){
-
-entry.target.style.opacity = 1
-entry.target.style.transform = "translateY(0)"
-
-}
-
-})
-
-})
-
-
-document.querySelectorAll(".card,.counter").forEach(el => {
-
-el.style.opacity = 0
-el.style.transform = "translateY(40px)"
-el.style.transition = "all 0.6s ease"
-
-observer.observe(el)
-
-})
-
-
-
-var map = L.map('map').setView([-22.92,-45.46],12)
+var map=L.map('map').setView([-22.92,-45.46],12)
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+
 attribution:'© OpenStreetMap'
+
 }).addTo(map)
 
+
 L.marker([-22.92,-45.46]).addTo(map)
-.bindPopup("Centro de atuação do Fundo Social")
+
+.bindPopup("Centro do Fundo Social")
